d7ade3514c5b06c5f27cbca7a74e032b15bfd5be
Blaž Strušnik <42406427+Blaz-Strusnik@users.noreply.github.com>
2023-09-05 13:29:01 +0200

    Update ubuntu_install_script.sh

--- ubuntu_install_script.sh
+++ ubuntu_install_script.sh
@@ -27,6 +27,7 @@ sudo systemctl status docker
 sudo apt install postgresql postgresql-contrib
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt install pgadmin4-web
 sudo /usr/pgadmin4/bin/setup-web.sh
 firefox "http://127.0.0.1/pgadmin4"
 sudo apt install mysql-server -y
