f27d5880877a489e4ac634c379dd71a158dae084
Blaž Strušnik <42406427+Blaz-Strusnik@users.noreply.github.com>
2023-09-05 12:51:42 +0200

    First commit

---
+++ ubuntu_install_script.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo systemctl status postgresql
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -O- https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/vscode.gpg
+echo deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.microsoft.com/repos/vscode stable main | sudo tee /etc/apt/sources.list.d/vscode.list
+sudo apt update
+sudo apt install code
+code
+sudo apt-get install nautilus-dropbox -y
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+sudo apt update
+sudo apt install gh -y
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+source ~/.profile
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-cache policy docker-ce
+sudo apt install docker-ce
+sudo systemctl status docker
+curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+
+sudo /usr/pgadmin4/bin/setup-web.sh
+firefox "http://127.0.0.1/pgadmin4"
+sudo apt install mysql-server -y
+wget -P /home/blaz/Documents https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
+cd ~/Documents
+chmod +x Anaconda3-2022.10-Linux-x86_64.sh
+./Anaconda3-2022.10-Linux-x86_64.sh
+mkdir /home/blaz/Documents/Projects
+mkdir /home/blaz/Documents/v_environment
+# sudo apt install postgresql postgresql-contrib
+sudo docker version
+nvm --version
+gh --version
+code --version
+mysql -V
+node -v
