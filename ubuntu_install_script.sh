35d250c4eb2f16c62c4cef95292f5558f9f0f274
Blaž Strušnik <42406427+Blaz-Strusnik@users.noreply.github.com>
2023-09-06 14:34:05 +0200

    Update ubuntu_install_script.sh

--- ubuntu_install_script.sh
+++ ubuntu_install_script.sh
@@ -31,6 +31,11 @@ curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compo
 sudo apt update
 chmod +x ~/.docker/cli-plugins/docker-compose
 sudo apt update
+sudo gpasswd -a $USER docker
+newgrp docker
+sudo apt update
+sudo su $USER
+sudo apt update
 sudo apt install postgresql postgresql-contrib
 curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
