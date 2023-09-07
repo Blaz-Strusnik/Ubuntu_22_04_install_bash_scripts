7f7633c2b3a6d12a0e7d9dbb937cce6ba3c5975e
Blaž Strušnik <42406427+Blaz-Strusnik@users.noreply.github.com>
2023-09-07 11:50:09 +0200

    Update ubuntu_install_script.sh

--- ubuntu_install_script.sh
+++ ubuntu_install_script.sh
@@ -56,3 +56,19 @@ code --version
 mysql -V
 node -v
 docker compose version
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+sudo apt update
+cd ~/Documents
+wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz
+tar -xf Python-3.9.1.tgz
+cd Python-3.9.1
+./configure --enable-optimizations
+make -j 12
+sudo make altinstall
+python3.9 --version
+sudo apt install python3-venv -y
+mkdir cd ~/Documents/v_environment/
+cd ~/Documents/v_environment/
+python3.9 -m venv "newenv"
+source ~/Documents/v_environment/newenv/bin/activate
+deactivate
